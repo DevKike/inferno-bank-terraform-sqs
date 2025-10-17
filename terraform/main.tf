b02d8d368fe7a68e18a1612a712a5ab4b4a3ab0c
DevKike <alvaronarvaez.dev@gmail.com>
2025-10-17 00:24:12 -0500

    feat: add check balance and transaction sqs configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,17 @@ resource "aws_sqs_queue" "start_payment_sqs" {
   visibility_timeout_seconds = 30
   message_retention_seconds  = 345600
 }
+
+resource "aws_sqs_queue" "check_balance_sqs" {
+  name                       = "check-balance-sqs"
+  delay_seconds              = 0
+  visibility_timeout_seconds = 30
+  message_retention_seconds  = 345600
+}
+
+resource "aws_sqs_queue" "transaction_sqs" {
+  name                       = "transaction-sqs"
+  delay_seconds              = 0
+  visibility_timeout_seconds = 30
+  message_retention_seconds  = 345600
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,19 @@ output "start_payment_sqs_url" {
 output "start_payment_sqs_arn" {
   value = aws_sqs_queue.start_payment_sqs.arn
 }
+
+output "check_balance_sqs_url" {
+  value = aws_sqs_queue.start_payment_sqs.id
+}
+
+output "check_balance_sqs_arn" {
+  value = aws_sqs_queue.check_balance_sqs.arn
+}
+
+output "transaction_sqs_url" {
+  value = aws_sqs_queue.start_payment_sqs.id
+}
+
+output "transaction_sqs_arn" {
+  value = aws_sqs_queue.check_balance_sqs.arn
+}
